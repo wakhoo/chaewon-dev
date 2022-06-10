aa204f3701ca7e4cad676be5d2a9ef8ea025ed38
wakhoo <qqww212@korea.ac.kr>
2022-06-10 16:48:33 +0900

    blog part

--- services/index.js
+++ services/index.js
@@ -55,7 +55,7 @@ export const getPostDetails = async (slug) => {
           createdAt
           slug
           content {
-            raw
+           raw
           }
           categories {
             name
@@ -127,3 +127,71 @@ export const getSimilarPosts = async (categories, slug) => {
   
     return result.categories;
   };
+
+
+  export const submitComment = async (obj) => {
+    const result = await fetch('/api/comments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(obj),
+    });
+  
+    return result.json();
+  };
+
+
+  export const getComments = async (slug) => {
+    const query = gql`
+      query GetComments($slug:String!) {
+        comments(where: {post: {slug:$slug}}){
+          name
+          createdAt
+          comment
+        }
+      }
+    `;
+  
+    const result = await request(graphqlAPI, query, { slug });
+  
+    return result.comments;
+  };
+
+  export const getCategoryPost = async (slug) => {
+    const query = gql`
+      query GetCategoryPost($slug: String!) {
+        postsConnection(where: {categories_some: {slug: $slug}}) {
+          edges {
+            cursor
+            node {
+              author {
+                bio
+                name
+                id
+                photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              title
+              excerpt
+              featuredImage {
+                url
+              }
+              categories {
+                name
+                slug
+              }
+            }
+          }
+        }
+      }
+    `;
+  
+    const result = await request(graphqlAPI, query, { slug });
+  
+    return result.postsConnection.edges;
+  };
+  
