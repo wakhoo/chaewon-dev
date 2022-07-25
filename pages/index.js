57d68837ce2058bea3788ead91c9ea1972d54013
wakhoo <qqww212@korea.ac.kr>
2022-07-26 00:06:06 +0900

    index.js modify

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,34 @@
 import NavBar from '../components/NavBar'
 import Seo from '../components/Seo';
-export default function Home(){
+import { PostCard,PostWidget,Categories } from "../components"
+import { getPosts } from "../services"
+export default function Home({posts}){
   return(
    
-    <div>
-      <Seo title="Home"/>
-      <h1>Hello</h1>
-
+    <div className="container mx-auto px-10 mb-8" >
+    <Seo title="Blog"/>
+   <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+       <div className="lg:col-span-8 col-span-1">
+        {posts.map((post)=><PostCard post={post.node} key={post.title}/>)}    
+       </div>
       
+    <div className="lg:col-span-4 col-span-1">
+            <div className="lg:sticky relative top-8">
+                <PostWidget/>
+                <Categories/>
+            </div>
     </div>
+    </div>
+</div>
 
   );
+}
+
+export async function getStaticProps(){
+  const posts=(await getPosts()) || [];
+
+  return{
+      props:{posts}
+  }
+
 }
